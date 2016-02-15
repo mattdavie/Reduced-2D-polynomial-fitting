c932a3677200d50e9d10ddd6d48385c88ee463f0
Matt Davie <m.davie@student.unsw.edu.au>
2016-02-15 13:45:32 +1100

    Create reduced_poly2D.py
    Fitting large 2d polynomials has a lot of gross-terms which can lead to irritating degeneracy in your solutions, lack of convergence in mcmc simulations etc. Nobody really wants this, and whilst an iterative approach where one sequentially omits low magnitude cross-terms and then refits would be possible, it is unpleasantly inefficient. This is a customisable, if blunt approach.

---
+++ reduced_poly2D.py
@@ -0,0 +1,25 @@
+import numpy as np
+import itertools
+
+
+def reducedpolyfit2D(x,y,z,ord=6,crosslim=2):
+	#ord will fit up to x**(ord-1) * y**(ord-1)
+	#and limit cross terms where a both powers > crosslim
+	ncols=(ord+1)**2
+	G=np.zeros((x.size,ncols))
+	ij=itertools.product(range(ord+1),range(ord+1))
+	for k,(i,j) in enumerate(ij):
+		G[:,k]=x**i * y**j * (i<crosslim or j<crosslim)
+	m,_,_,_=np.linalg.lstsq(G,z)
+	return m
+	
+def reducedpolyval2D(x,y,m,crosslim=2):
+  #will return the value of your fitted polynomial whose coefficients are m
+  #at coordinates (x,y) again limiting crossterms as before
+	ord=int(np.sqrt(len(m)))-1
+	ij=itertools.product(range(ord+1),range(ord+1))
+	z=np.zeros_like(x)
+	for a,(i,j) in zip (m, ij):
+		print i,j,a
+		z+=a* x**i * y**j * (i<crosslim or j<crosslim)
+	return z
