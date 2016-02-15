e1536acd4088ecd256922272aa451a35aaaaa5a3
Matt Davie <m.davie@student.unsw.edu.au>
2016-02-15 16:10:38 +1100

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -3,7 +3,7 @@ import mumpy as np
 numdata=180
 x=.5-np.random.random(numdata)*1.
 y=.5-np.random.random(numdata)*1.
-z=-1 +x -2*y**3 +3*x**2 -4*x*y**2 +5*y*x**3 +6*x*y**5 -7*x**5#+np.random.random(numdata)
+z=-1 +x -2*y**3 +3*x**2 -4*x*y**2 +5*y*x**3 +6*x*y**5 -7*x**5#+np.random.random(numdata)*0.05
 m=reducedpolyfit2D(x,y,z,ord=5)
 nx,ny=100,100
 xx,yy=np.meshgrid(np.linspace(x.min(),x.max(),nx),np.linspace(y.min(),y.max(),ny))
